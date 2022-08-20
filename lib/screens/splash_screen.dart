f173d1c6fdb1cb3a0290f14783cd0214992b1d3d
Amber Jain <jainanuamber@gmail.com>
2022-08-21 01:36:55 +0530

    updated

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,15 +1,88 @@
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatefulWidget {
-  SplashScreen({Key? key}) : super(key: key);
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
 
-  @override
-  State<SplashScreen> createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.only(top: 75, left: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: const [
+                        Text(
+                          "Spot",
+                          style: TextStyle(
+                              fontSize: 30, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          "N",
+                          style: TextStyle(
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.amber),
+                        ),
+                        Text(
+                          "ote",
+                          style: TextStyle(
+                              fontSize: 30, fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      padding: const EdgeInsets.only(top: 25),
+                      child: const Text(
+                        "Create and design your notes easily 🗒️",
+                        style: TextStyle(fontSize: 25),
+                      ),
+                    ),
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      child: Image.asset(
+                        'assets/images/splash_screen_image.png',
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 75),
+              SizedBox(
+                width: 180.0,
+                height: 50.0,
+                child: ElevatedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.amber.shade400),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0),
+                        ))),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Text(
+                          "Let's Go",
+                          style: TextStyle(color: Colors.black, fontSize: 18),
+                        ),
+                        Icon(
+                          Icons.lock_outline_rounded,
+                          color: Colors.black,
+                        ),
+                      ],
+                    )),
+              ),
+              const SizedBox(width: 50),
+            ],
+          ),
+        ));
   }
 }

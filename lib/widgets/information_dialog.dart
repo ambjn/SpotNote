8c6462033735c918f9c8bb8b29851b67ed6bb182
Amber Jain <jainanuamber@gmail.com>
2022-08-21 14:02:57 +0530

    information_dialog added

---
+++ lib/widgets/information_dialog.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class InformationDialog extends StatelessWidget {
+  InformationDialog({Key? key, this.radius = 8, required this.dialogData})
+      : super(key: key);
+
+  final double radius;
+  var dialogData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      shape:
+          RoundedRectangleBorder(borderRadius: BorderRadius.circular(radius)),
+      child: SizedBox(
+        height: 500,
+        child: Column(children: [
+          Expanded(
+            child: FutureBuilder(
+              future: Future.delayed(const Duration(milliseconds: 150)),
+              builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+                return dialogData;
+              },
+            ),
+          ),
+          TextButton(
+              style: ButtonStyle(
+                  shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(radius),
+                          bottomRight: Radius.circular(radius)))),
+                  padding: MaterialStateProperty.all(const EdgeInsets.all(8)),
+                  backgroundColor: MaterialStateProperty.all(Colors.teal)),
+              onPressed: (() => Get.back()),
+              child: Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(radius),
+                        bottomRight: Radius.circular(radius))),
+                alignment: Alignment.center,
+                height: 35,
+                width: double.infinity,
+                child: const Text(
+                  "OK",
+                  style: TextStyle(fontSize: 20, color: Colors.white),
+                ),
+              ))
+        ]),
+      ),
+    );
+  }
+}
